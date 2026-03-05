bff08ffc942e6dff5cbbe923d0766e9aee18725d
wuxi <wuxi@sailingwell.com.cn>
2026-03-05 17:26:05 +0800

    12

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { createApp } from 'vue'
 import './assets/css/reset.css'
 import App from './App.vue'
 import { Icon } from "@iconify/vue";
+import { createPinia } from 'pinia'
 import router from './router'
+const pinia = createPinia()
 const app = createApp(App)
+
+
 app.component("Icon", Icon);
 app.use(router)
+app.use(pinia)
 app.mount('#app').$nextTick(() => {
   // Use contextBridge
   window.ipcRenderer.on('main-process-message', (_event, message) => {
